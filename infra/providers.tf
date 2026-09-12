80d227a9a75432f2d0b72b3bec46fa5bef51323f
wrehman1 <wrehman1091@gmail.com>
2026-09-12 11:56:45 +0100

    add infra folder

---
+++ infra/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_version = ">= 1.10.0"
+
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 6.62"
+      }
+    }
+}
+
+provider "aws" {
+    region = var.aws_region
+
+    default_tags {
+      tags = {
+        Project     = var.project_name
+        Environment = var.environment
+        ManagedBy   = "Terraform"
+      }
+    }
+}
