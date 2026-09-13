7d467812ed6f34852c340160229a63b64103fed3
wrehman1 <wrehman1091@gmail.com>
2026-09-13 12:56:53 +0100

    add outputs.tf

---
+++ infra/modules/Network/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "VPC ID."
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDS."
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnets_ids" {
+  description = "Private subnet IDS."
+  value       = module.vpc.private_subnets
+}
+
+output "nat_gateway_ids" {
+  description = "NAT Gateway IDS"
+  value       = module.vpc.natgw_ids  
+}
