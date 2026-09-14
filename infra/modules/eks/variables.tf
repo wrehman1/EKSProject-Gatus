6ffe7f8549a84d0ff1775f0adff2009566e20453
wrehman1 <wrehman1091@gmail.com>
2026-09-14 17:52:36 +0100

    add eks folder

---
+++ infra/modules/eks/variables.tf
@@ -0,0 +1,41 @@
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version."
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "Private subnet IDs used by EKS."
+  type        = list(string)
+}
+
+variable "worker_role_arn" {
+  description = "Existing IAM role ARN used by worker nodes."
+  type        = string
+}
+
+variable "node_instance_types" {
+  description = "EC2 instance types used by worker nodes."
+  type        = list(string) 
+}
+
+variable "node_min_size" {
+  type = number
+}
+
+variable "node_desired_size" {
+  type = number
+}
+
+variable "node_max_size" {
+  type = number
+}
