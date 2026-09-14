d7c347482f6f31e56ee8988c1baba4222b46ceca
wrehman1 <wrehman1091@gmail.com>
2026-09-14 17:43:16 +0100

    amended outputs.tf

--- infra/modules/worker-iam/outputs.tf
+++ infra/modules/worker-iam/outputs.tf
@@ -1,9 +1,9 @@
 output "role_arn" {
   description = "Worker node IAM role ARN."
-  value = aws_iam_role.worker.arn
+  value       = aws_iam_role.worker.arn
 }
 
 output "role_name" {
   description = "Worker node IAM role name."
-  value = aws_iam_role.worker.name
+  value       = aws_iam_role.worker.name
 }
