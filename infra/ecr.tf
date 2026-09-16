44a903cfa437bd32fd064aaa68dc1946f893db8d
wrehman1 <wrehman1091@gmail.com>
2026-09-16 17:28:52 +0100

    add ecr.tf

---
+++ infra/ecr.tf
@@ -0,0 +1,37 @@
+resource "aws_ecr_repository" "gatus" {
+  name = "${local.name}-app"
+
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = AES256
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "gatus" {
+  repository = aws_ecr_repository.gatus.name
+
+  policy = jsondecode({
+    rules = [
+        {
+            rulePriority = 1
+
+            description = "Keep the most recent 30 images"
+
+            selection = {
+                tagStatus   = "any"
+                countType   = "imageCountMoreThan"
+                countNumber = 30
+            }
+
+            action = {
+                type = "expire"
+            }
+        }
+    ]
+  })
+}
