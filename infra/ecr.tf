6645d652955c212eb9618f72d3a5efd2d6da4946
wrehman1 <wrehman1091@gmail.com>
2026-09-24 16:47:47 +0100

    made amendments

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -8,30 +8,30 @@ resource "aws_ecr_repository" "gatus" {
   }
 
   encryption_configuration {
-    encryption_type = AES256
+    encryption_type = "AES256"
   }
 }
 
 resource "aws_ecr_lifecycle_policy" "gatus" {
   repository = aws_ecr_repository.gatus.name
 
-  policy = jsondecode({
+  policy = jsonencode({
     rules = [
-        {
-            rulePriority = 1
+      {
+        rulePriority = 1
 
-            description = "Keep the most recent 30 images"
+        description = "Keep the most recent 30 images"
 
-            selection = {
-                tagStatus   = "any"
-                countType   = "imageCountMoreThan"
-                countNumber = 30
-            }
-
-            action = {
-                type = "expire"
-            }
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 30
         }
+
+        action = {
+          type = "expire"
+        }
+      }
     ]
   })
 }
