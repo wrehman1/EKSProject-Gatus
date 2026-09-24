9e7c35f5d8f80830ebf45a3058bddb9f41a9ad6f
wrehman1 <wrehman1091@gmail.com>
2026-09-24 16:43:39 +0100

    amended name

---
+++ infra/modules/vpc/main.tf
@@ -0,0 +1,33 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.7.2"
+
+  name = var.name
+  cidr = var.vpc_cidr
+
+  azs = var.availability_zones
+
+  public_subnets = var.public_subnets
+
+  private_subnets = var.private_subnets
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = var.single_nat_gateway
+
+  map_public_ip_on_launch = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+}
---
+++ infra/modules/vpc/variables.tf
@@ -0,0 +1,35 @@
+variable "name" {
+  description = "Name prefix for network resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block used by the VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "Availability Zones."
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDRs."
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDRs."
+  type        = list(string)
+}
+
+variable "cluster_name" {
+  description = "EKS Cluster name used for Kubernetes subnet discovery."
+  type        = string
+}
+
+variable "single_nat_gateway" {
+  description = "Whether to provision one shared NAT Gateway."
+  type        = bool
+  default     = true
+}
